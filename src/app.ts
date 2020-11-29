65d59da51d5808d7bbc1cbc59d0d1dc72cfcc8e7
prashant <prashant@astradot.com>
2020-11-29 09:25:42 -0800

    initial

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,6 @@ const fs = require('fs');
 const chalk = require('chalk');
 const yargs = require('yargs');
 const {resolve} = require("path");
-const dedent = require('dedent-js');
 
 interface ICLIOptions {
     config: string,
@@ -22,6 +21,7 @@ interface IKafaTopicConfig {
 interface IYamlConfig {
     bootstrapServers: string,
     replication: number,
+    defaultCompression: string,
     topics: IKafaTopicConfig[],
 }
 
@@ -32,49 +32,12 @@ function hoursToMs(hours: number) {
     return ms;
 }
 
-function createKafkaCommand(topic: IKafaTopicConfig, kafka: IYamlConfig) {
-    const compression = topic.compression ? topic.compression : "zstd";
-    const retentionMs = hoursToMs(topic.retentionHours);
-
-    return `$KTOOLS_HOME/kafka-topics.sh -bootstrap-server "${kafka.bootstrapServers}" --create --replication-factor ${kafka.replication}  --config compression.type=${compression} --config retention.ms=${retentionMs} --topic ${topic.name}`;
-}
-
-
-function createBashFile(commandList: string[], toolsBaseDir: string, bashScriptFile: string) {
-    const cmdContents = commandList.join("\n\n");
-
-    const bashContents = `
-            #!/bin/bash
-            
-            # https://twitter.com/b0rk/status/1314345978963648524/photo/1
-            set -euo pipefail
-            
-            KTOOLS_HOME='${toolsBaseDir}'
-            
-            ${cmdContents}
-            
-            echo "All Kafka Config Done"
-    `;
-
-    fs.writeFile(bashScriptFile, dedent(bashContents), (err: any) => {
-        if (err) throw err;
-        console.log(chalk.green(`Done writing bash file ${chalk.white(bashScriptFile)}`)); // Success
-    });
-}
-
 function main() {
     const options: ICLIOptions = yargs
         .usage("Usage: --config <config.yaml>")
         .option("c", {alias: "config", describe: "configuration yaml", type: "string", demandOption: true})
-        .option("k", {alias: "kdir", describe: "directory containing kafka tools binaries", type: "string", demandOption: true})
-        .option("o", {alias: "outfile", describe: "file to output bash script to", type: "string", demandOption: true})
         .argv;
 
-    const kDir = resolve(options.kdir);
-    if (!fs.existsSync(kDir)) {
-        console.log(chalk.red("directory doesnt exist"), kDir);
-        return;
-    }
     const yamlFile = options.config;
     if (!fs.existsSync(yamlFile)) {
         console.log(chalk.red("file doesnt exist"), yamlFile);
@@ -82,10 +45,6 @@ function main() {
     }
     const yamlContents = fs.readFileSync(yamlFile);
     const kafkaConfig: IYamlConfig = yaml.safeLoad(yamlContents);
-
-    const topicCmds = kafkaConfig.topics.map(topic => createKafkaCommand(topic, kafkaConfig));
-
-    createBashFile(topicCmds, kDir, options.outfile);
 }
 
 main();
